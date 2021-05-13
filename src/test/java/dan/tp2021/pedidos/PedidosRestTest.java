706132f28361fcf59585efdae196ff56e063c1ae
Martin Perussini <mperussini@frsf.utn.edu.ar>
2021-05-13 15:50:36 -0300

    Testing Hechos los test de crear pedido a nivel capa rest

---
+++ src/test/java/dan/tp2021/pedidos/PedidosRestTest.java
@@ -0,0 +1,128 @@
+package dan.tp2021.pedidos;
+
+import dan.tp2021.pedidos.domain.DetallePedido;
+import dan.tp2021.pedidos.domain.Obra;
+import dan.tp2021.pedidos.domain.Pedido;
+import dan.tp2021.pedidos.domain.Producto;
+import dan.tp2021.pedidos.exceptions.cliente.ClienteException;
+import dan.tp2021.pedidos.exceptions.cliente.ClienteNoHabilitadoException;
+import dan.tp2021.pedidos.services.PedidoService;
+import dan.tp2021.pedidos.services.PedidoServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+//@SpringBootApplication
+//@Profile("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PedidosRestTest {
+    private TestRestTemplate testRestTemplate = new TestRestTemplate();
+    @LocalServerPort
+    String puerto;
+
+    Pedido unPedido;
+    Obra unaObra;
+    DetallePedido unDetallePedido, otroDetallePedido;
+    Producto unProducto, otroProducto;
+
+    @MockBean
+    PedidoService pedidoServiceImpl;
+
+    @BeforeEach
+    void setUp(){
+        unPedido = new Pedido();
+        unaObra = new Obra();
+        unDetallePedido = new DetallePedido();
+        otroDetallePedido = new DetallePedido();
+        unProducto = new Producto();
+        otroProducto = new Producto();
+        unPedido.setDetalle(new ArrayList<>());
+
+        unaObra.setId(1);
+        unaObra.setDescripcion("Obra de prueba");
+        unDetallePedido.setProducto(unProducto);
+        unDetallePedido.setCantidad(5);
+        unDetallePedido.setPrecio(150.85);
+        otroDetallePedido.setProducto(otroProducto);
+        otroDetallePedido.setCantidad(20);
+        otroDetallePedido.setPrecio(124.20);
+
+        unPedido.setObra(unaObra);
+        unPedido.getDetalle().add(unDetallePedido);
+        unPedido.getDetalle().add(otroDetallePedido);
+    }
+
+    @Test
+    void crearPedidoCorrecto() throws ClienteException, ClienteNoHabilitadoException {
+        when(pedidoServiceImpl.savePedido(any(Pedido.class))).thenReturn(unPedido);
+        String server = "http://localhost:"+puerto+"/api/pedido";
+        HttpEntity<Pedido> requestCliente = new HttpEntity<>(unPedido);
+        ResponseEntity<String> response = testRestTemplate.exchange(server, HttpMethod.POST,requestCliente,String.class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+    }
+
+    @Test
+    void crearPedidoSinObra() throws ClienteException, ClienteNoHabilitadoException {
+        when(pedidoServiceImpl.savePedido(any(Pedido.class))).thenReturn(unPedido);
+        unPedido.setObra(null);
+        String server = "http://localhost:"+puerto+"/api/pedido";
+        HttpEntity<Pedido> requestCliente = new HttpEntity<>(unPedido);
+        ResponseEntity<String> response = testRestTemplate.exchange(server, HttpMethod.POST,requestCliente,String.class);
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+    }
+
+    @Test
+    void crearPedidoSinDetalles() throws ClienteException, ClienteNoHabilitadoException {
+        when(pedidoServiceImpl.savePedido(any(Pedido.class))).thenReturn(unPedido);
+        unPedido.setDetalle(null);
+        String server = "http://localhost:"+puerto+"/api/pedido";
+        HttpEntity<Pedido> requestCliente = new HttpEntity<>(unPedido);
+        ResponseEntity<String> response = testRestTemplate.exchange(server, HttpMethod.POST,requestCliente,String.class);
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+    }
+
+    @Test
+    void crearPedidoConDetallesSinContenido() throws ClienteException, ClienteNoHabilitadoException {
+        when(pedidoServiceImpl.savePedido(any(Pedido.class))).thenReturn(unPedido);
+        unPedido.setDetalle(new ArrayList<>());
+        String server = "http://localhost:"+puerto+"/api/pedido";
+        HttpEntity<Pedido> requestCliente = new HttpEntity<>(unPedido);
+        ResponseEntity<String> response = testRestTemplate.exchange(server, HttpMethod.POST,requestCliente,String.class);
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+    }
+
+    @Test
+    void crearPedidoConUnDetalleSinProducto() throws ClienteException, ClienteNoHabilitadoException {
+        when(pedidoServiceImpl.savePedido(any(Pedido.class))).thenReturn(unPedido);
+        unPedido.getDetalle().get(0).setProducto(null);
+        String server = "http://localhost:"+puerto+"/api/pedido";
+        HttpEntity<Pedido> requestCliente = new HttpEntity<>(unPedido);
+        ResponseEntity<String> response = testRestTemplate.exchange(server, HttpMethod.POST,requestCliente,String.class);
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+    }
+
+    @Test
+    void crearPedidoConUnDetalleSinCantidad() throws ClienteException, ClienteNoHabilitadoException {
+        when(pedidoServiceImpl.savePedido(any(Pedido.class))).thenReturn(unPedido);
+        unPedido.getDetalle().get(1).setCantidad(null);
+        String server = "http://localhost:"+puerto+"/api/pedido";
+        HttpEntity<Pedido> requestCliente = new HttpEntity<>(unPedido);
+        ResponseEntity<String> response = testRestTemplate.exchange(server, HttpMethod.POST,requestCliente,String.class);
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+    }
+}
