5d2b8c537040987a9b8f194df9bab05dfd4f84a6
Gaston <gaston.diaz.contractor@macmillan.com>
2021-05-29 19:17:33 -0300

    Archivos de testing

---
+++ src/test/resources/datos_iniciales.sql
@@ -0,0 +1,10 @@
+create schema `dan-ms-pedido`;
+
+insert into `dan-ms-pedido`.estado_pedido (id, estado) values (1,'NUEVO');
+insert into `dan-ms-pedido`.estado_pedido (id, estado) values (2,'CONFIRMADO');
+insert into `dan-ms-pedido`.estado_pedido (id, estado) values (3,'PENDIENTE');
+insert into `dan-ms-pedido`.estado_pedido (id, estado) values (4,'CANCELADO');
+insert into `dan-ms-pedido`.estado_pedido (id, estado) values (5,'ACEPTADO');
+insert into `dan-ms-pedido`.estado_pedido (id, estado) values (6,'RECHAZADO');
+insert into `dan-ms-pedido`.estado_pedido (id, estado) values (7,'EN PREPARACION');
+insert into `dan-ms-pedido`.estado_pedido (id, estado) values (8,'ENTREGADO');
